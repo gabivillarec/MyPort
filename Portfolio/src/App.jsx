31628655d6b9b7e8c9fcaccd49825281812af9ad
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-10-17 17:47:53 -0300

    estilos reseteados y componentes creados

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
+import style from './App.module.css';
+//import Header from './Header/Header';
+//import AsideRight from './AsideRight/AsideRight';
+//import AsideLeft from './AsideLeft/AsideLeft';
 
 
 function App() {
 
-
   return (
-    <>
+    <div className={style.container}>
+
       <h1>PortFolio</h1>
-    </>
+
+      
+    </div>
   )
 }
 
