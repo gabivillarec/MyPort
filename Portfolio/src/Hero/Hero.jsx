e40d239093a45a781eed8825a187985056f5bfc9
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-10-18 18:01:40 -0300

    estilos del home finalizados , faltan solo animaciones y trabajar en el about, projects y botones de redes y mail

--- Portfolio/src/Hero/Hero.jsx
+++ Portfolio/src/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () =>{
             <p className={style.saludo}>Hi, my name is</p>
             <h1 className={style.name}>Gabriel Villalobos.</h1>
             <h1 className={style.prof}>FullStack Developer and Winemaker.</h1>
-            <p className={style.description}>I am a web developer with a love for the world of wine. My professional journey has been woven between lines of code and vineyards, merging my technological expertise with my passion for winemaking. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project. Welcome to my world, where technology and wine come together!</p>
+            <p className={style.description}>I'm a web developer with a love for the world of wine. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project.</p>
             <button className={style.btn}>Projects</button>
         </div>
     )
