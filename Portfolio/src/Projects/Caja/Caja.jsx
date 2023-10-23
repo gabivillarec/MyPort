0d6fde178a6cbc217dc63d784cf17d79baa56063
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-10-23 18:47:52 -0300

    arreglo de caja

--- Portfolio/src/Projects/Caja/Caja.jsx
+++ Portfolio/src/Projects/Caja/Caja.jsx
@@ -4,9 +4,13 @@ import style from './caja.module.css'
 const Caja = () =>{
 
     return(
+
         <div className={style.box}>
+            
             <h1>1</h1>
+
         </div>
+
     )
 };
 
