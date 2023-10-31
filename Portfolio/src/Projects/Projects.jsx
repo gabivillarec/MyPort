eaab8d60064f3c866ff6a988f6dc5ce66fee680c
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-10-31 19:55:30 -0300

    creé archivo data.js y lo importe en projects.jsx

--- Portfolio/src/Projects/Projects.jsx
+++ Portfolio/src/Projects/Projects.jsx
@@ -1,19 +1,9 @@
-import style from './projects.module.css'
+import style from './projects.module.css';
 import { useState, useEffect } from "react";
 import AsideLeft from "../Home/AsideLeft/AsideLeft";
+import data from './data';
 import Caja from './Caja/Caja';
-import rick from './assets/rick.png'
-import city from './assets/city.jpg'
-import liliana from './assets/liliana.png'
 
-const arrayPro = [
-    {id:1 , title:"Rick and Morty", link:"https://github.com/gabivillarec/Proyecto-RaM", img:rick, 
-    texto:"lorem lksahfglakshf lkashflkashf lkashflk asfhlkashf lkashflaksh flkashflk"},
-    {id:2 , title:"City of Games", link:"https://github.com/gabivillarec/PI-Videogames-main", img:city, 
-    texto:"lorem  lksahfglak shflkash flkashf lkashflkas fhlkashf lkashflak shflkashflk"},
-    {id:3 , title:"Liliana Games", link:"https://github.com/gabivillarec/Liliana-GameStore", img:liliana, 
-    texto:"lorem  lksahfglakshf lkashflkashfl kashflkasfh lkashflka shflak shflkashflk"}
-] 
 
 const Projects = () =>{   
 
@@ -31,7 +21,7 @@ const Projects = () =>{
         <div className={style.container}>
             <AsideLeft/>
             <div className={`${style.pro} ${isVisible ? style.fadeIn : ''}`}>
-               {arrayPro.map(el=>{
+               {data.map(el=>{
                 return(
                     <Caja
                     key= {el.id}
---
+++ Portfolio/src/Projects/data.js
@@ -0,0 +1,32 @@
+import rick from './assets/rick.png'
+import city from './assets/city.jpg'
+import liliana from './assets/liliana.png'
+
+
+const data = [
+    {   
+        id:1, 
+        title:"Rick and Morty", 
+        link:"https://github.com/gabivillarec/Proyecto-RaM", 
+        img:rick, 
+        texto:"lorem lksahfglakshf lkashflkashf lkashflk asfhlkashf lkashflaksh flkashflk"
+    },
+
+    {   
+        id:2, 
+        title:"City of Games", 
+        link:"https://github.com/gabivillarec/PI-Videogames-main", 
+        img:city, 
+        texto:"lorem  lksahfglak shflkash flkashf lkashflkas fhlkashf lkashflak shflkashflk"
+    },
+
+    {   
+        id:3, 
+        title:"Liliana Games", 
+        link:"https://github.com/gabivillarec/Liliana-GameStore", 
+        img:liliana, 
+        texto:"lorem  lksahfglakshf lkashflkashfl kashflkasfh lkashflka shflak shflkashflk"
+    }
+];
+
+export default data;
