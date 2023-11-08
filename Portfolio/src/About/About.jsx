37dd5e7459fe4db528b24ab4290b05b4b3eeba43
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-11-08 19:18:30 -0300

    Finalizacion de About.css

--- Portfolio/src/About/About.jsx
+++ Portfolio/src/About/About.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import style from './about.module.css'
 import AsideLeft from "../Home/AsideLeft/AsideLeft";
 import yo from "./assets/yo.png"
+import analitica from "./assets/analitica.png"
 
 let css3 = "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg";
 let html5 = "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg";
@@ -30,13 +31,12 @@ const About = () =>{
             <AsideLeft/>
             <div className={`${style.resume} ${isVisible ? style.fadeIn : ''}`}>
                 <div className={style.text}>
-                    <h1>Título del about</h1>
-                    <p className={style.acerca}>Greetings, I am a winemaker and web developer with a passion for the world of wine and a strong background in web development.
+                    <p className={style.acerca}><span className={style.greet}>Greetings</span>, I am a <em>web developer</em> and <em>winemaker</em> with a passion for the world of wine and a strong background in web development.
                     <p>My fascination with chemistry led me to explore the universe of wine. I have acquired all the necessary knowledge to approach winemaking professionally, having undertaken multiple projects involving homemade wines beyond commercial products.</p>
                     <p>However, oenology is not the only facet that defines me. From an early age, technology, especially computers, has been my passion. I became a self-taught individual in this field, providing software maintenance services to various clients who rely on my expertise to address their IT needs.</p>
-                    <p>My foray into web development was driven by curiosity, as it was not initially the branch of informatics that attracted me the most. In 2020, I had my first encounter with a programming language, JavaScript. At that time, I found it challenging to grasp, as I lacked knowledge of HTML and CSS. Nevertheless, I dedicated myself to learning these fundamentals and prepared to join HENRY, where I participated in a bootcamp that expanded my knowledge.</p>
+                    <p>My foray into web development was driven by curiosity, as it was not initially the branch of informatics that attracted me the most. In 2020, I had my first encounter with a programming language, <em>JavaScript</em>. At that time, I found it challenging to grasp, as I lacked knowledge of <em>HTML</em> and <em>CSS</em>. Nevertheless, I dedicated myself to learning these fundamentals and prepared to join <a href='https://www.soyhenry.com/' target='_blank'rel="noreferrer" className={style.henry} >HENRY</a>, where I participated in a bootcamp that expanded my knowledge.</p>
                     <p>Currently, working with programming languages has opened up new horizons for me and motivated me to continue developing in this field. I am eager to keep learning and exploring this exciting and challenging world.</p>
-                    <p>If you would like to learn more about my career, please do not hesitate to review my <a href='https://drive.google.com/file/d/1-hn3YACH5dcoomUyMhsqyf5sZUDITVJN/view?usp=drive_link' target='_blank'rel="noreferrer">CV</a>.</p></p>
+                    <p>If you would like to learn more about my career, please do not hesitate to review my <a className={style.cv} href='https://drive.google.com/file/d/1-hn3YACH5dcoomUyMhsqyf5sZUDITVJN/view?usp=drive_link' target='_blank'rel="noreferrer">CV</a>.</p></p>
                                         
                     <div className={style.logoBox}> 
                         <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
@@ -59,6 +59,7 @@ const About = () =>{
                 
                 </div>
                 <img src={yo} alt="Gabriel Villalobos" className={style.yo} />
+                <img src={analitica} className={style.ana}></img>
             </div>
         </div>
     )
