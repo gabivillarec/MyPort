d381575c3da410f8db10c3d4c3a0e05baf4cfa66
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-11-14 20:43:11 -0300

    proyecto finalizado

--- Portfolio/src/Projects/data.js
+++ Portfolio/src/Projects/data.js
@@ -9,7 +9,7 @@ const data = [
         title:"Rick and Morty", 
         link:"https://github.com/gabivillarec/Proyecto-RaM", 
         img:rick, 
-        texto: "Uso de API, login de usuario, búsquedas, filtros, ordenamiento, formularios controlados, Redux y estados, rutas, controllers y base de datos. Primer proyecto individual asignado por Henry para evaluar competencias implementadas dentro de todo el bootcamp."
+        texto: "Use of API, user login, searches, filters, sorting, controlled forms, Redux and states, routes, controllers, and database. First individual project assigned by Henry to evaluate competencies implemented within the entire bootcamp."
     },
 
     {   
@@ -17,7 +17,7 @@ const data = [
         title:"City of Games", 
         link:"https://github.com/gabivillarec/PI-Videogames-main", 
         img:city, 
-        texto:"Uso de API, búsquedas, filtros, ordenamiento, formularios controlados, Redux y estados, rutas, controllers y base de datos. Segundo proyecto individual asignado por Henry para evaluar competencias implementadas dentro de todo el bootcamp."
+        texto:"Use of API, searches, filters, sorting, controlled forms, Redux and states, routes, controllers, and database. Second individual project assigned by Henry to evaluate competencies implemented within the entire bootcamp."
     },
 
     {   
@@ -25,7 +25,7 @@ const data = [
         title:"Liliana Games", 
         link:"https://github.com/gabivillarec/Liliana-GameStore", 
         img:liliana, 
-        texto:"Login, mercado pago, carrito de compras, búsquedas, filtros, ordenamiento, formularios controlados, Redux y estados, rutas, controllers y base de datos. Primer proyecto grupal elaborado por 6 participantes, por medio de GitHub y organizado en Jira"
+        texto:"Login, Mercado Pago, shopping cart, searches, filters, sorting, controlled forms, Redux and states, routes, controllers, and database. First group project developed by 6 participants, through GitHub and organized in Jira."
     }
 ];
 
