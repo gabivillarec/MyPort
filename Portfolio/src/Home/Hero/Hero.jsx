d6209651885d73cd157a1841ef5407202b99f50d
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-11-16 14:19:40 -0300

    hero

--- Portfolio/src/Home/Hero/Hero.jsx
+++ Portfolio/src/Home/Hero/Hero.jsx
@@ -1,5 +1,5 @@
-import style from './hero.module.css'
 import { useNavigate } from 'react-router-dom';
+import style from './hero.module.css'
 
 
 const Hero = () =>{
@@ -12,7 +12,7 @@ const Hero = () =>{
             <p className={style.saludo}>Hi, my name is</p>
             <h1 className={style.name}>Gabriel Villalobos.</h1>
             <h1 className={style.prof}>FullStack Developer and Winemaker.</h1>
-            <p className={style.description}>I'm a web developer with a love for the world of wine. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project.</p>
+            <p className={style.description}>I am a web developer with a love for the world of wine. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project.</p>
             <button className={style.btn} onClick={()=>navigate('/projects')}>Projects</button>
             <button className={style.btnH} onClick={()=>navigate('/about')}>About</button>
         </div>
