10877f917ff936bbccb4f7ad33e68377f25f1754
Gabriel Villalobos <bl_182_bl@hotmail.com>
2023-11-16 14:39:17 -0300

    hero

--- Portfolio/src/Home/Hero/Hero.jsx
+++ Portfolio/src/Home/Hero/Hero.jsx
@@ -8,16 +8,15 @@ const Hero = () =>{
 
 
     return(
-        <>
-        <div className={style.container}>
+
+        <div className={style.contHero}>
             <p className={style.saludo}>Hi, my name is</p>
             <h1 className={style.name}>Gabriel Villalobos.</h1>
             <h1 className={style.prof}>FullStack Developer and Winemaker.</h1>
-            <p className={style.description}>I am a web developer with a love for the world of wine. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project.</p>
+            <p className={style.description}>I'm a web developer with a love for the world of wine. As a developer, my focus is on creating exceptional digital experiences. This duality in my life inspires me to blend creativity and precision in every project.</p>
             <button className={style.btn} onClick={()=>navigate('/projects')}>Projects</button>
             <button className={style.btnH} onClick={()=>navigate('/about')}>About</button>
         </div>
-        </>
     )
 };
 
